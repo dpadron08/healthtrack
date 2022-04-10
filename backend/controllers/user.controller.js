d7c93ffcc70d5bfcc6283d6cc1026e835fa0523f
David Padron Castillo <padroncastillo@gmail.com>
2022-04-10 01:50:10 -0400

    Create basic backend user sign up functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,21 +1,59 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const User = require("../models/user.model")
+
 // @desc    Register a user
 // @route   POST /api/users/
 // @access  Public
-const registerUser = (req, res) => {
-    res.json({message: "Register User"})
+const registerUser = async (req, res) => {
+    const { first_name, last_name, email, password } = req.body
+    if (!first_name || !last_name || !email || !password) {
+        res.json({message: "Please add all fields"})
+        return
+        // throw new Error("Please add all fields")
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({email})
+    if (userExists) {
+        res.json({message: "User already exists"})
+        return
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = await User.create({
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email
+        })
+    } else {
+        res.json("Invalid user data or user failed to create")
+    }
 }
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
     res.json({message: "Login User"})
 }
 
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Private
-const getMe = (req, res) => {
+const getMe = async (req, res) => {
     res.json({message: "Seeing user data"})
 }
 
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,21 @@ const express = require("express")
 const cors = require("cors")
 const { userRouter } = require("./routes/user.route.js")
 const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 dotenv.config()
 
 const app = express()
-const port = process.env.port
+const port = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect(process.env.URI), {useNewUrlParser: true}
+const db = mongoose.connection;
+db.on("error", () => console.log("database connection failed"))
+db.once("open", () => console.log("database connection successful"))
+
 
 app.get("/", (req, res) => res.json({message: "Hello world!"}))
 
@@ -18,4 +26,6 @@ app.use("*", (req, res) => {
     res.status(404).json({error: "Not found!"})
 })
 
-app.listen(port)
+app.listen(port, () => {
+    console.log("App is running on port " + port)
+})
