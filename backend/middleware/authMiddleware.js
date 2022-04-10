115bece41c0d4684cc283f3869aeb803ada9a4c3
David Padron Castillo <padroncastillo@gmail.com>
2022-04-10 14:34:11 -0400

    Add basic user authentication middleware for protecting routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/user.model")
+
+const protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        try {
+            // get token from header
+            token = req.headers.authorization.split(" ")[1]
+            // verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            // get user from token
+            req.user = await User.findById(decoded.id).select("-password");
+            next()
+        } catch (e) {
+            console.log("Error in protect jwt token" + e);
+            console.log("User not authorized");
+        }
+    }
+
+    if (!token) {
+        res.status(401).json("Not authorized, no token produced")
+    }
+}
+
+module.exports = {
+    protect
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const { registerUser, loginUser, getMe } = require("../controllers/user.controller.js")
+const { protect } = require("../middleware/authMiddleware")
 const userRouter = express.Router()
 
 userRouter.post("/", registerUser)
 userRouter.post("/login", loginUser)
-userRouter.get("/me", getMe)
+userRouter.get("/me", protect, getMe)
 
 module.exports = {
     userRouter
