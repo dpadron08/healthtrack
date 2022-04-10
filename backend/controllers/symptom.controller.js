db7e8fb2350007354d645ec50e0bb22294d21e11
David Padron Castillo <padroncastillo@gmail.com>
2022-04-10 17:11:49 -0400

    Set up initial REST API for symptoms

--- backend/controllers/symptom.controller.js
+++ backend/controllers/symptom.controller.js
@@ -4,28 +4,52 @@ const Symptom = require("../models/symptom.model")
 // @route   GET /api/symptoms/
 // @access  Private
 const getSymptoms = async (req, res) => {
-  res.status(200).json({ message: "Seeing symptoms" });
+  const symptoms = await Symptom.find()
+  res.status(200).json( symptoms );
 };
 
 // @desc    Submit symptoms
 // @route   POST /api/symptoms/
 // @access  Private
 const setSymptoms = async (req, res) => {
-  res.status(200).json({ message: "Setting symptoms" });
+  console.log("Symptom request body text: " + req.body.text)
+  const symptom = await Symptom.create({
+    text: req.body.text
+  })
+  if (symptom) {
+    res.status(200).json(symptom);
+  } else {
+    res.status(400).json({message: "Could not create symptom"})
+  }
 };
 
 // @desc    Edit a symptom
 // @route   PUT /api/symptom/:id
 // @access  Private
 const updateSymptom = async (req, res) => {
-  res.status(200).json({ message: "Updating a symptom" });
+  const symptom = await Symptom.findById(req.params.id)
+  if (!symptom) {
+    res.status(400).json({message: "Symptom id not found"})
+    return
+  }
+  if (!req.body.text) {
+    res.status(400).json("Please include a text with the symptom")
+  }
+  const updatedSymptom = await Symptom.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  res.status(200).json(updatedSymptom);
 };
 
 // @desc    Delete a symptom
 // @route   DELETE /api/symptom/:id
 // @access  Private
 const deleteSymptom = async (req, res) => {
-  res.status(200).json({ message: "Deleting a symptom" });
+  const symptom = await Symptom.findById(req.params.id)
+  if (!symptom) {
+    res.status(400).json("No symptom of this id found to delete")
+    return
+  }
+  await symptom.remove()
+  res.status(200).json({ id: req.params.id });
 };
 
 module.exports = {
