4ab02eaa6a87435fa56ec256efdc287172ca3f8e
David Padron Castillo <padroncastillo@gmail.com>
2022-04-14 17:32:46 -0400

    Format project

--- frontend/src/services/symptomService.js
+++ frontend/src/services/symptomService.js
@@ -1,57 +1,57 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_URL = "http://localhost:8000" + "/api/symptoms/"
+const API_URL = "http://localhost:8000" + "/api/symptoms/";
 
 // create a new symptom
 const createSymptom = async (symptomData, token) => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
-    }
-  }
-  
-  const response = await axios.post(API_URL, symptomData, config)
-  return response.data
-}
+    },
+  };
+
+  const response = await axios.post(API_URL, symptomData, config);
+  return response.data;
+};
 
 const getSymptoms = async (token) => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
-    }
-  }
+    },
+  };
 
-  const response = await axios.get(API_URL, config)
-  return response.data
-}
+  const response = await axios.get(API_URL, config);
+  return response.data;
+};
 
 const deleteSymptom = async (symptomId, token) => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
-    }
-  }
+    },
+  };
 
-  const response = await axios.delete(API_URL + symptomId, config)
-  return response.data
-}
+  const response = await axios.delete(API_URL + symptomId, config);
+  return response.data;
+};
 
 const editSymptom = async (symptomId, token) => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
-    }
-  }
+    },
+  };
 
-  const response = await axios.put(API_URL + symptomId, config)
-  return response.data
-}
+  const response = await axios.put(API_URL + symptomId, config);
+  return response.data;
+};
 
 const symptomService = {
   createSymptom,
   getSymptoms,
   deleteSymptom,
-  editSymptom
-}
+  editSymptom,
+};
 
 export default symptomService;
