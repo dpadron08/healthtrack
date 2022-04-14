590305801fac166d1661ef9eb9a0c49c4dfbe4d7
David Padron Castillo <padroncastillo@gmail.com>
2022-04-13 20:25:20 -0400

    Create react frontent

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello, world
+    </div>
+  );
+}
+
+export default App;
