28adc3d1a6957a128b5335229bf1ac90fbeb8fd1
David Padron Castillo <padroncastillo@gmail.com>
2022-04-18 17:04:13 -0400

    Implement user registrations

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import authService from "../services/authService";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -12,7 +14,35 @@ function Register() {
 
   const { first_name, last_name, email, password1, password2 } = formData;
 
-  const onChange = () => {};
+  const navigate = useNavigate();
+
+  const onChangeField = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!(formData.password1 === formData.password2)) {
+      alert("Passwords dont match");
+      return;
+    }
+
+    const userData = {
+      first_name: formData.first_name,
+      last_name: formData.last_name,
+      email: formData.email,
+      password: formData.password1,
+    };
+
+    authService.register(userData).then((response) => {
+      if (response.message) {
+        alert(response.message);
+      } else {
+        navigate("/", { replace: true });
+      }
+    });
+  };
 
   return (
     <form>
@@ -24,9 +54,9 @@ function Register() {
           id="first_name"
           name="first_name"
           value={first_name}
-          aria-describedby="emailHelp"
+          aria-describedby="firstNameHelp"
           placeholder="Enter first name"
-          onChange={onChange}
+          onChange={onChangeField}
         />
       </div>
       <div className="form-group">
@@ -37,9 +67,9 @@ function Register() {
           id="last_name"
           name="last_name"
           value={last_name}
-          aria-describedby="emailHelp"
+          aria-describedby="lastNameHelp"
           placeholder="Enter last name"
-          onChange={onChange}
+          onChange={onChangeField}
         />
       </div>
       <div className="form-group">
@@ -52,7 +82,7 @@ function Register() {
           value={email}
           aria-describedby="emailHelp"
           placeholder="Enter email"
-          onChange={onChange}
+          onChange={onChangeField}
         />
         <small id="emailHelp" className="form-text text-muted">
           We'll never share your email with anyone else.
@@ -68,7 +98,7 @@ function Register() {
           name="password1"
           value={password1}
           placeholder="Enter password"
-          onChange={onChange}
+          onChange={onChangeField}
         />
       </div>
       <div className="form-group">
@@ -80,10 +110,10 @@ function Register() {
           name="password2"
           value={password2}
           placeholder="Confirm password"
-          onChange={onChange}
+          onChange={onChangeField}
         />
       </div>
-      <button type="submit" className="btn btn-primary">
+      <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
         Submit
       </button>
     </form>
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -6,9 +6,15 @@ const API_URL = "http://localhost:8000" + "/api/users/";
 const register = async (userData) => {
   const response = await axios.post(API_URL + "register", userData);
   if (response.data) {
-    localStorage.setItem("user", JSON.stringify(response.data));
+    // valid server responses won't have a message, so if one is
+    // found, exit and display the error message. Note that this
+    // pattern is not true for the login route
+    if (!response.data.message) {
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
   }
-  return response.data;
+  return { message: "Unable to register from authService" };
 };
 
 // login user
