2d750a060998f76c606869e3bd6d5da98ca6fce1
David Padron Castillo <padroncastillo@gmail.com>
2022-04-20 16:52:20 -0400

    Format project

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,32 @@
-const express = require("express")
-const cors = require("cors")
-const { userRouter } = require("./routes/user.route.js")
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
-const symptomRouter = require("./routes/symptom.route.js")
-dotenv.config()
+const express = require("express");
+const cors = require("cors");
+const { userRouter } = require("./routes/user.route.js");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const symptomRouter = require("./routes/symptom.route.js");
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.URI, {useNewUrlParser: true})
+mongoose.connect(process.env.URI, { useNewUrlParser: true });
 
 const db = mongoose.connection;
-db.on("error", () => console.log("database connection failed"))
-db.once("open", () => console.log("database connection successful"))
+db.on("error", () => console.log("database connection failed"));
+db.once("open", () => console.log("database connection successful"));
 
-
-app.get("/", (req, res) => res.json({message: "Hello world!"}))
-app.use("/api/users", userRouter)
-app.use("/api/symptoms", symptomRouter)
+app.get("/", (req, res) => res.json({ message: "Hello world!" }));
+app.use("/api/users", userRouter);
+app.use("/api/symptoms", symptomRouter);
 
 app.use("*", (req, res) => {
-    res.status(404).json({error: "Not found!"})
-})
+  res.status(404).json({ error: "Not found!" });
+});
 
 app.listen(port, () => {
-    console.log("App is running on port " + port)
-})
+  console.log("App is running on port " + port);
+});
